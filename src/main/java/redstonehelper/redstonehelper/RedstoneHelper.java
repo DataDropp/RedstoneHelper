a8e26cb8335019af642e0d7ec38490dae08164bc
Chad Oliveira <lvnvceo@gmail.com>
2020-10-29 00:16:39 -0700

    Added configuration support

--- src/main/java/redstonehelper/redstonehelper/RedstoneHelper.java
+++ src/main/java/redstonehelper/redstonehelper/RedstoneHelper.java
@@ -7,10 +7,17 @@ public final class RedstoneHelper extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        Bukkit.getLogger().info("[RedstoneHelper] Hello bukkit! :)");
-        getServer().getPluginManager().registerEvents(new Reverser(),this);
-        getServer().getPluginManager().registerEvents(new Filler(),this);
         this.saveDefaultConfig();
+        this.reloadConfig();
+        if(this.getConfig().getBoolean("enabled")) {
+            Bukkit.getLogger().info("[RedstoneHelper] Hello bukkit! :)");
+            if (this.getConfig().getBoolean("reverse.enabled")){
+                getServer().getPluginManager().registerEvents(new Reverser(this), this);
+            }
+            if(this.getConfig().getBoolean("fillables.enabled")){
+                getServer().getPluginManager().registerEvents(new Filler(this),this);
+            }
+        }
     }
     @Override
     public void onDisable() {
